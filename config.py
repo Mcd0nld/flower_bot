aa79fe839eb39918a609a110059dd6d2f43e3b02
mcd0nld <scarecrow0608@gmail.com>
2025-02-19 00:17:40 +0300

    сохраняю изменения перед pull

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
 import os
 from dotenv import load_dotenv
+
+# загружаем из .env
+load_dotenv()
+
+# токен бота
+TOKEN = os.getenv("BOT_TOKEN")
+
+# подключение к бд PostgreSQL
+DATABASE_URL =  os.getenv("DATABASE_URL")
+
+# телеграм оператора
+OPERATOR_CHAT_ID = os.getenv("OPERATOR_CHAT_ID")
+
+
