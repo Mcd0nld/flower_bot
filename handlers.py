b947291e415b749f69559397d8eeab14cc848487
mcd0nld <scarecrow0608@gmail.com>
2025-02-19 01:21:33 +0300

    улучшен обработчик

--- handlers.py
+++ handlers.py
@@ -1,5 +1,8 @@
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import (
+    ContextTypes,
+    ConversationHandler,
+)
 from sqlalchemy.orm import sessionmaker
 from database import engine, Customer
 from config import OPERATOR_CHAT_ID
@@ -7,22 +10,25 @@ from config import OPERATOR_CHAT_ID
 # cоздаем сессию для работы с бд
 Session = sessionmaker(bind=engine)
 
+# определяем состояния
+ASK_NAME, ASK_BIRTHDAY, ASK_PHONE = range(3)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Приветствие пользователя"""
     await update.message.reply_text("Привет! Как тебя зовут?")
-    return "ASK_NAME"
+    return ASK_NAME
 
 async def ask_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Запрос имени пользователя"""
     context.user_data["name"] = update.message.text
     await update.message.reply_text("Отлично! Теперь укажи свою дату рождения (ГГГГ-ММ-ДД):")
-    return "ASK_BIRTHDAY"
+    return ASK_BIRTHDAY
 
 async def ask_birthday(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Запрос даты рождения пользователя"""
     context.user_data["birth_date"] = update.message.text
     await update.message.reply_text("Теперь введи свой номер телефона (+7):")
-    return "ASK_PHONE"
+    return ASK_PHONE
 
 async def ask_phone(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Запрос номера телефона и сохранение данных пользователя"""
@@ -46,4 +52,6 @@ async def ask_phone(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
     await update.message.reply_text("Спасибо! Наш оператор скоро с вами свяжется. @FiorePerAmore1")
-    return "END"
+    return ConversationHandler.END
+
+
